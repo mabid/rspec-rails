f916a87d383568fc71e173c8338632dc085ce72e
Abid Mahmood <mabid.mahmood@gmail.com>
2011-09-19 20:53:28 +0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lpc::Application.config.secret_token = 'bd8070bb49d1dbcdc7c10832319954d489ef86596c4e09b0d47be6ef94bbfbc7cf7ad6c787f09f08a6a1896f10c2db31582231e07d89fef8ddf2d7e49c731d75'
